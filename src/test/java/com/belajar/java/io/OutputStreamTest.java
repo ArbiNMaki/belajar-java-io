b18c22d1fd43b7f5041510c4c64ed62788514267
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-27 09:05:20 +0700

    Open Option

--- src/test/java/com/belajar/java/io/OutputStreamTest.java
+++ src/test/java/com/belajar/java/io/OutputStreamTest.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class OutputStreamTest {
 
@@ -15,6 +16,21 @@ public class OutputStreamTest {
         Path path = Path.of("output.txt");
 
         try (OutputStream stream = Files.newOutputStream(path)) {
+            for (int i = 0; i < 100; i++) {
+                stream.write("Halo Dunia\n".getBytes());
+                stream.flush();
+            }
+        } catch (IOException e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
+    void testAppendWithOpenOption() {
+        Path path = Path.of("append.txt");
+
+        try (OutputStream stream = Files.newOutputStream(path,
+                StandardOpenOption.CREATE, StandardOpenOption.APPEND)) {
             for (int i = 0; i < 100; i++) {
                 stream.write("Halo Dunia\n".getBytes());
                 stream.flush();
